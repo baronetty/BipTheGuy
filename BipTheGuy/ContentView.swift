ec2f3187d94be4bbd943c47364766257ad4a6c83
Leo Barnett <leo.e.barnett@icloud.com>
2023-12-13 18:18:00 +0200

    Version 1.0

--- BipTheGuy/ContentView.swift
+++ BipTheGuy/ContentView.swift
@@ -5,17 +5,43 @@
 //  Created by Leo  on 13.12.23.
 //
 
+import AVFAudio
 import SwiftUI
 
 struct ContentView: View {
+    @State private var audioPlayer: AVAudioPlayer!
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            Image("clown")
+                .resizable()
+                .scaledToFit()
+                .onTapGesture {
+                    playSound(soundName: "punchSound")
+                }
+            Spacer()
+            
+            Button {
+                
+            } label: {
+                Label("Photo Library", systemImage: "photo.fill.on.rectangle.fill")
+            }
         }
         .padding()
+    }
+    
+    func playSound(soundName: String) {
+        guard let soundFile = NSDataAsset(name: soundName) else {
+            print("🤬 Could not read file named \(soundName)")
+            return
+        }
+        do {
+            audioPlayer = try AVAudioPlayer(data: soundFile.data)
+            audioPlayer.play()
+        } catch {
+            print("🤬 ERROR: \(error.localizedDescription) creating audioPlayer")
+        }
     }
 }
 
