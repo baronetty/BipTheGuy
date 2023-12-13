8ddca98d93e7aad48ba8d4f49d9e3b75f5fe84ce
Leo Barnett <leo.e.barnett@icloud.com>
2023-12-13 17:59:05 +0200

    Initial Commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by Leo  on 13.12.23.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
