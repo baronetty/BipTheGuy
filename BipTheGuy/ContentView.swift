f4fa3c2323465fa2daf82e4a76a567ad37a5b964
Leo Barnett <leo.e.barnett@icloud.com>
2023-12-16 17:54:04 +0200

    Version 1.0

--- BipTheGuy/ContentView.swift
+++ BipTheGuy/ContentView.swift
@@ -10,16 +10,24 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var audioPlayer: AVAudioPlayer!
+    @State private var animateImage = true
     
     var body: some View {
         VStack {
             Spacer()
+            
             Image("clown")
                 .resizable()
                 .scaledToFit()
+                .scaleEffect(animateImage ? 1.0 : 0.9)
                 .onTapGesture {
-                    playSound(soundName: "punchSound")
+//                    playSound(soundName: "punchSound")
+                    animateImage = false // will immediately shrink using .scaleEffect to 90% of size
+                    withAnimation(.spring(response: 0.3, dampingFraction: 0.3)) {
+                        animateImage = true // will go from 90% to 100% size with spring animation
+                    }
                 }
+            
             Spacer()
             
             Button {
